2719798561b3cf5667519c1e8ddcce94addf165b
Prateek <prateek.sachdeva@svyamultimedia.com>
2019-10-24 09:34:46 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { WeddingComponent } from './wedding/wedding.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeddingComponent
   ],
   imports: [
     BrowserModule
